2bae11af682eea7111ab6f8b84f35e2f47fa360e
karenr.romero8 <karenr.romero8@906-27.bmcc.cuny.edu>
2019-09-14 12:18:10 -0400

    This is the first commit for the git project in netbeans

---
+++ Test/src/test/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author karenr.romero8
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Git in Netbeans works!");
+    }
+}
